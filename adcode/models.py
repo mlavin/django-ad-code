16d4964f8015929f30c6f224570374f657c4ef36
mlavin <markdlavin@gmail.com>
2012-05-06 15:54:36 -0400

    Initial models and migration.

--- adcode/models.py
+++ adcode/models.py
@@ -1,3 +1,28 @@
+"Models for managing site sections and ad placements."
+
 from django.db import models
 
-# Create your models here.
+
+class Section(models.Model):
+    "A grouping of site urls."
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    pattern = models.CharField(max_length=200)
+
+
+class Size(models.Model):
+    "Common Ad size."
+
+    name = models.CharField(max_length=100)
+    width = models.PositiveSmallIntegerField()
+    height = models.PositiveSmallIntegerField()
+
+
+class Placement(models.Model):
+    "Ad to be rendered in given sections."
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    size = models.ForeignKey(Size, related_name='placements')
+    sections = models.ManyToManyField(Section, related_name='placements')
