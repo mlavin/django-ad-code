d21b5c3fa2c3a85db23db7be3bc381564dcafc78
mlavin <markdlavin@gmail.com>
2012-05-09 22:17:47 -0400

    Starting next release.

--- adcode/__init__.py
+++ adcode/__init__.py
@@ -3,4 +3,4 @@ django-ad-code is a reusable application for managing and rendering ad tags
 from ad networks or exchanges such Adsense, DoubleClick or OpenX.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0dev'
