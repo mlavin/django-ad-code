f7ce22cb14512b08243388bdfac20689b2f06d6e
mlavin <markdlavin@gmail.com>
2012-05-16 20:15:53 -0400

    Test fixture loading.

--- adcode/tests/__init__.py
+++ adcode/tests/__init__.py
@@ -1,3 +1,4 @@
+from .base import FixturesTestCase
 from .context_processors import CurrentPlacementsTestCase
 from .performance import QueryCountsTestCase
 from .templatetags import RenderHeaderTestCase, RenderPlacementTestCase
