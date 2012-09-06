0f0be5c864e511ede06632d8c043e5ca094782fe
Mark Lavin <markdlavin@gmail.com>
2012-09-06 10:33:03 -0400

    Another unicode_literals for good measure.

--- adcode/__init__.py
+++ adcode/__init__.py
@@ -2,5 +2,6 @@
 django-ad-code is a reusable application for managing and rendering ad tags 
 from ad networks or exchanges such Adsense, DoubleClick or OpenX.
 """
+from __future__ import unicode_literals
 
 __version__ = '0.4.0dev'
