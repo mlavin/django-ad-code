ad0fc5bdbf239e2dedeb524252be3f4bc4909e47
mlavin <markdlavin@gmail.com>
2018-04-21 13:11:43 -0400

    Version bump and release notes. New classifiers.

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,20 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
 
 from django.views.generic.base import TemplateView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'example.views.home', name='home'),
-    # url(r'^example/', include('example.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-)
+]
